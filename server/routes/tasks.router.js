0f4b4fdc101912dbc70f2e5879abb3feccb7bf95
Brianna Oliveira <mystikgab@gmail.com>
2022-08-14 19:57:47 -0500

    In tasks.router.js, create GET to request tasks from the server

---
+++ server/routes/tasks.router.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+const pool = require('../modules/pool.js');  
+
+router.get('/', (req, res) => {
+    // Query to run
+    const queryText = 'SELECT * FROM "tasks";';
+    pool.query(queryText).then((result) => {
+        // SELECT returns rows
+        res.send(result.rows);
+    }).catch((error) => {
+        console.log('Error in GET /tasks', error);
+        res.sendStatus(500);
+    });
+});
